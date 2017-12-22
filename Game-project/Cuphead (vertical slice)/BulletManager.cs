e808863c273dc4a586f591c75b63d804c78a7d25
Doyrd <22474@ma-web.nl>
2017-12-22 15:40:44 +0100

    Cuphead scripts

---
+++ Game-project/Cuphead (vertical slice)/BulletManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+
+    public float xTravelSpeed;
+    public float yTravelSpeed;
+    public float damageValue;
+
+    void Start ()
+    {
+        StartCoroutine(DestroyBulletsAfterTime(10));
+	}
+	
+	void Update ()
+    {
+        Vector2 position = transform.position;
+        position.x += xTravelSpeed;
+        position.y += yTravelSpeed;
+        transform.position = position;
+	}
+
+    //Destroying the bullet after certain time
+    IEnumerator DestroyBulletsAfterTime(float timeLimit)
+    {
+        yield return new WaitForSeconds(timeLimit);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Game-project/Cuphead (vertical slice)/CollectableManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableManager : MonoBehaviour
+{
+
+    private Animator theAnimator;
+
+    public GameObject theCoinBackground;
+
+	void Start ()
+    {
+        theAnimator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(CoinFinished(0.75f));
+            theCoinBackground.SetActive(false);
+            theAnimator.SetBool("Is picked up", true);
+            /*gameObject.SetActive(false);*/
+        }
+    }
+
+    IEnumerator CoinFinished(float theTimeToWait)
+    {
+        yield return new WaitForSeconds(theTimeToWait);
+        gameObject.SetActive(false);
+    }
+}
